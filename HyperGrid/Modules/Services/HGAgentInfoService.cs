705797f498fa8f1b94b427b745b2d3608ee48ed7
JAllard <JuniorAllard@gmail.com>
2012-05-26 18:36:02 +0200

    fix tracked files

---
+++ HyperGrid/Modules/Services/HGAgentInfoService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Nini.Config;
+using Aurora.Framework;
+using OpenSim.Services.Interfaces;
+using OpenSim.Services;
+using OpenMetaverse;
+
+namespace Aurora.Addon.Hypergrid
+{
+    public class HGAgentInfoService : AgentInfoService
+    {
+        public override void Initialize (IConfigSource config, IRegistryCore registry)
+        {
+            m_registry = registry;
+            IConfig hgConfig = config.Configs["HyperGrid"];
+            if (hgConfig == null || !hgConfig.GetBoolean ("Enabled", false))
+                return;
+
+            IConfig handlerConfig = config.Configs["Handlers"];
+            if (handlerConfig.GetString ("AgentInfoHandler", "") != Name)
+                return;
+
+            registry.RegisterModuleInterface<IAgentInfoService>(this);
+            Init(registry, Name);
+        }
+
+        [CanBeReflected(ThreatLevel = ThreatLevel.Low)]
+        public override List<string> GetAgentsLocations(string requestor, List<string> userIDs)
+        {
+            List<string> locations = new List<string> ();
+            foreach (string userID in userIDs)
+            {
+                List<string> l = base.GetAgentsLocations(requestor, new List<string> { userID });
+                if (l[0] == "NotOnline")
+                {
+                    UserAccount acc = m_registry.RequestModuleInterface<IUserAccountService> ().GetUserAccount (UUID.Zero, UUID.Parse (userID));
+                    if (acc == null)
+                    {
+                        IUserFinder userFinder = m_registry.RequestModuleInterface<IUserFinder> ();
+                        string url = "";
+                        if (userFinder != null && (url = userFinder.GetUserServerURL (UUID.Parse (userID), GetHandlers.Helpers_IMServerURI)) != "")
+                            l[0] = url;
+                    }
+                }
+                locations.Add (l[0]);
+            }
+            return locations;
+        }
+    }
+}
